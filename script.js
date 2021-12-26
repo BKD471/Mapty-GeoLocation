f24041bd7361fcbf5c2852ef9f610a6f106d22c2
BKD471 <bhaskarkumardas9@gmail.com>
2021-12-26 18:38:39 +0530

    class speedworkoutcyc

--- script.js
+++ script.js
@@ -10,6 +10,53 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + "").slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; //[lat,lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in mn
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    //min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    //km/hr
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+
+// console.log(run1, cycling1);
+
+//////////////////////////////////////////////
+/////// APPLICATION ARCHITECTURE
+
 class App {
   #map;
   #mapEvent;
@@ -73,6 +120,7 @@ class App {
     L.marker([latC, lngC])
       .addTo(this.#map)
       .bindPopup(
+        //used to customize pop up marker
         L.popup({
           maxWidth: 250,
           minWidth: 100,
@@ -81,6 +129,7 @@ class App {
           className: "running-popup",
         })
       )
+      // used to set the content of marker
       .setPopupContent("Running")
       .openPopup();
   }
